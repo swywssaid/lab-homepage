8bf059fd72a4d0a516cb1e4cda5da60972dabd8a
swywssaid <swywssaid@gmail.com>
2023-10-04 07:27:13 +0900

    deploy to gh-pages

---
+++ out/_next/static/1Icj43WJa15e0jM3mTEly/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fpublications\u002F[...slug]","\u002Fpublications\u002Fpage\u002F[page]","\u002Ftags\u002F[tag]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ out/_next/static/chunks/app/publications/page/[page]/page-46ec9e206841a32b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[782],{},function(n){n.O(0,[724,427,971,864,744],function(){return n(n.s=3427)}),_N_E=n.O()}]);
